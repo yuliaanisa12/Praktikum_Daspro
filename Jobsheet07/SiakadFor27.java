1e7975f09e3a466af966a9ffc61474a57010698e
yuliaanisa12 <yuliaanisaputri0726@gmail.com>
2025-10-28 19:20:17 -0700

    jobsheet07

--- Jobsheet07/SiakadFor27.java
+++ Jobsheet07/SiakadFor27.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SiakadFor27 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("test");
 
         // variabel
         double nilai;
