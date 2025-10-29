5b46115dde9ab7f3a3bd220b3139edbe25daed3f
yuliaanisa12 <yuliaanisaputri0726@gmail.com>
2025-10-28 18:39:29 -0700

    jobsheet07

---
+++ Jobsheet07/SiakadFor27.java
@@ -0,0 +1,28 @@
+package Jobsheet07;
+
+import java.util.Scanner;
+
+public class SiakadFor27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // variabel
+        double nilai;
+        double tertinggi = 0;
+        double terendah = 100;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("masukkan nilai makasiswa ke-" + i + ": ");
+            nilai = sc.nextDouble();
+            if (nilai > tertinggi) {
+                tertinggi = nilai;
+            }
+            if (nilai < terendah) {
+                terendah = nilai;
+            }
+        }
+        System.out.println("nilai tertinggi:" + tertinggi);
+        System.out.println("nilai terendah: " + terendah);
+        sc.close();
+    }
+}
