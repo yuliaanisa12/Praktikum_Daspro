44a0eeef403f690bbb3b5d851e51289032ab6ed1
yuliaanisa12 <yuliaanisaputri0726@gmail.com>
2025-11-06 02:21:29 -0800

    Percobaan 1

---
+++ Jobsheet08/Square27.java
@@ -0,0 +1,20 @@
+package Jobsheet08;
+
+import java.util.Scanner;
+
+public class Square27{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("masukkan nilai n = ");
+        int n = sc.nextInt();
+        for (int i = 1; 1 <= n; i++) {
+            for (int j = 1; j<= n; j++) {
+                System.out.print("+");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
---
+++ Jobsheet08/Star27.java
@@ -0,0 +1,17 @@
+package Jobsheet08;
+
+import java.util.Scanner;
+
+public class Star27{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+     
+    System.out.print("masukkan nilai n = ");
+    int n = sc.nextInt();
+
+    for(int i = 0; i<=n; i++){
+        System.out.print("*");
+    }
+    System.out.println();
+    }
+}
