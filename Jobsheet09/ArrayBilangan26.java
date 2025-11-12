7aea33cd776a205285b6d1af808c329071579157
yuliaanisa12 <yuliaanisaputri0726@gmail.com>
2025-11-12 03:27:25 -0800

    Percobaan 1

---
+++ Jobsheet09/ArrayBilangan26.java
@@ -0,0 +1,16 @@
+package Jobsheet09;
+
+import java.util.Scanner;
+
+public class ArrayBilangan26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int []bil = {5,13,-7,17};
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+
+
+    }
+}
