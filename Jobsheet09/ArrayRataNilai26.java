da5ed3cc6fa12c45bb7a348ccf49cb36ca3430f8
yuliaanisa12 <yuliaanisaputri0726@gmail.com>
2025-11-13 19:01:45 -0800

    Percobaan 3

--- Jobsheet09/ArrayRataNilai26.java
+++ Jobsheet09/ArrayRataNilai26.java
@@ -5,22 +5,56 @@ import java.util.Scanner;
 public class ArrayRataNilai26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int [] nilaiMhs= new int [10];
-        int lulus = 0;
-        int tidakLulus = 0;
-        double total  = 0;
-        double rata2;
 
+        double total = 0, rata2;
+        double rataLulus = 0, rataTidakLulus = 0;
+        int jumlahLulus = 0;
+        int TdkLulus = 0;
+        int totalLulus = 0, totalTidakLulus = 0;
+
+        System.out.print("Masukan jumlah mahasiswa :");
+        int jumlahMahasiswa = sc.nextInt();
+
+        int[] nilaiMhs = new int[jumlahMahasiswa];
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("masukkan nilai mahasiswa ke-"+(i+1)+":");
+            System.out.print("Masukan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
-        }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                TdkLulus++;
             }
-        
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
-    }
 
+        }
+        if (jumlahLulus > 0) {
+            rataLulus = (double) totalLulus / jumlahLulus;
+        }
+
+        if (TdkLulus > 0) {
+            rataTidakLulus = (double) totalTidakLulus / TdkLulus;
+        }
+        int nilaiTertinggi = nilaiMhs[0];
+        int nilaiTerendah = nilaiMhs[0];
+        for (int i = 1; i < nilaiMhs.length; i++) {
+            if (nilaiMhs[i] > nilaiTertinggi) {
+                nilaiTertinggi = nilaiMhs[i];
+            }
+            if (nilaiMhs[i] < nilaiTerendah) {
+                nilaiTerendah = nilaiMhs[i];
+            }
+        }
+        rata2 = total / nilaiMhs.length;
+        System.out.println("Mahasiswa dengan nilai tertinggi = " + nilaiTertinggi);
+        System.out.println("Mahasiswa dengan nilai terendah = " + nilaiTerendah);
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+
+    }
 }
+    
+
+
+
+
