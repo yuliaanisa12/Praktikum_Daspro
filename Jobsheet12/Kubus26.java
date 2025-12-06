49d736ec9f8f6cdd3443260af1720be1ee6e4833
yuliaanisa12 <yuliaanisaputri0726@gmail.com>
2025-12-06 05:20:08 -0800

    Tugas 1

--- Jobsheet12/Kubus26.java
+++ Jobsheet12/Kubus26.java
@@ -1,5 +1,22 @@
 package Jobsheet12;
 
 public class Kubus26 {
-    
+     public static double hitungVolume(double sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    public static double hitungLuasPermukaan(double sisi) {
+        return 6 * sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        double sisi = 5; 
+
+        double volume = hitungVolume(sisi);
+        double luasPermukaan = hitungLuasPermukaan(sisi);
+
+        System.out.println("Sisi Kubus: " + sisi);
+        System.out.println("Volume Kubus: " + volume);
+        System.out.println("Luas Permukaan Kubus: " + luasPermukaan);
+    }
 }
